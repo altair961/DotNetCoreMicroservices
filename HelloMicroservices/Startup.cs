8fc7ecef17b630be0bfa10ef85440ba10294e1d4
altair961 <altair961@example.com>
2018-05-13 18:19:13 +0300

    Added NuGet package "Nancy"

--- HelloMicroservices/Startup.cs
+++ HelloMicroservices/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Nancy.Owin;
 
 namespace HelloMicroservices
 {
@@ -20,15 +21,18 @@ namespace HelloMicroservices
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseOwin(buildFunc =>
+                buildFunc.UseNancy()
+            );
+            // if (env.IsDevelopment())
+            // {
+            //     app.UseDeveloperExceptionPage();
+            // }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            // app.Run(async (context) =>
+            // {
+            //     await context.Response.WriteAsync("Hello World!");
+            // });
         }
     }
 }
