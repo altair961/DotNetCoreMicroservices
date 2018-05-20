a801f70246723792df23cdffee62bac5af7ec723
altair961 <altair961@example.com>
2018-05-20 12:49:41 +0300

    Cleaned up

--- ShoppingCart/ShoppingCart/ShoppingCartModule.cs
+++ ShoppingCart/ShoppingCart/ShoppingCartModule.cs
@@ -5,33 +5,7 @@ namespace ShoppingCart.ShoppingCart
     using Nancy.ModelBinding;
 
     public class ShoppingCartModule : NancyModule
-    {
-        //public ShoppingCartModule(IShoppingCartStore shoppingCartStore) : base("/shoppingcart")
-        // {
-        //     Get("/{userId:int}", parameters =>
-        //     {
-        //         var userId = (int) parameters.userId;
-        //         //return shoppingCartStore.Get(userId);
-        //         return 5;
-        //     });        
-
-        //     //Get("/", _ => DateTime.UtcNow);
-        // }
-
-        // public ShoppingCartModule()
-        // {
-        //     Get("/", _ => DateTime.UtcNow);
-        // }
-     /*   
-        public ShoppingCartModule()
-        {
-            Get("/", parameters => 
-            { 
-                return DateTime.UtcNow; 
-            });
-        }
-        */
-          
+    {          
         public ShoppingCartModule() : base("/shoppingcart")
         {
             Get("/{userid:int}", parameters => 
