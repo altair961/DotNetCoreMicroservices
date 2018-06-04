af585802a20f101388b8f868c13cf4bd0713ca8a
altair961 <altair961@users.noreply.github.com>
2018-06-04 21:27:49 +0300

    Started to implement Post for SmsNotifierModule

---
+++ NotificationSystem/SmsNotifier/SmsModule.cs
@@ -0,0 +1,20 @@
+namespace NotificationSystem.SmsModule
+{
+    using Nancy;
+    using Nancy.ModelBinding;
+    public class SmsNotifierModule : NancyModule
+    {
+        public SmsNotifierModule()
+        {
+            Post("smsnotifier/{smsid:int}",
+                async (parameters, _) =>
+                {
+                    var smsId = (int)parameters.smsid;
+
+                    var message = this.Bind<SmsNotification>();
+
+                    return smsId.ToString();
+                });
+        }
+    }
+}
